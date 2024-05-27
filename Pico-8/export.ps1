9f93fe196aa3f34ea9f81090b4a2e5476dd63ca0
josiahhsu <77859877+josiahhsu@users.noreply.github.com>
2024-05-26 22:22:39 -0500

    Add export script
    I discovered that the export command can be run from the host OS instead of from within the Pico-8 console. This means I can streamline exports even more via a script!

---
+++ Pico-8/export.ps1
@@ -0,0 +1,9 @@
+$carts = ls *.p8
+foreach($cart in $carts)
+{
+    $cartname = (Get-Item $cart).BaseName
+    write-host $("Exporting " + $cartname)
+
+    pico8 $cart -export $($cartname + ".js")
+    pico8 $cart -export $("-l " + $cartname + ".png")
+}
