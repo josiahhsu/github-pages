9fe5115b2c50fbd37586d5cf597890b4f0da55d6
josiahhsu <77859877+josiahhsu@users.noreply.github.com>
2024-07-14 23:13:07 -0400

    move exported items to their own directory
    The logical next step.

--- Pico-8/export.ps1
+++ Pico-8/export.ps1
@@ -4,6 +4,6 @@ foreach($cart in $carts)
     $cartname = (Get-Item $cart).BaseName
     write-host $("Exporting " + $cartname)
 
-    pico8 $cart -export $($cartname + ".js")
-    pico8 $cart -export $("-l " + $cartname + ".png")
+    pico8 $cart -export $("exports/" + $cartname + ".js")
+    pico8 $cart -export $("-l exports/" + $cartname + ".png")
 }
